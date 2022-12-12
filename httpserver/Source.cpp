370b65dbbc3e75e0a1d2125242c82a1f8a8b20f9
Ömer YILDIZ <muttakigenc@gmail.com>
2022-12-12 20:29:41 +0300

    Add project files.

---
+++ httpserver/Source.cpp
@@ -0,0 +1,19 @@
+#ifdef linux
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#else
+#include <winsock.h>
+#endif
+
+//
+
+int main() {
+	
+	//
+	return 0;
+}
